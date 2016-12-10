17d38536bcf134ced4f4e460dd0cbe7643217c72
HammZ <henryhamon@gmail.com>
2016-12-10 01:43:45 +0000

    archive note

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::NotesController < BaseApiControllerController
-  before_filter :find_project, only: [:show, :update]
+  before_filter :find_note
   
   def create
-    @note = Note.create(params[:note])
 		if @note.valid?
 		  # change the project state to started
 		  (@note.project.notes.count<2 && @note.project.new?) ? @note.start : true
@@ -11,5 +10,19 @@ class Api::V1::NotesController < BaseApiControllerController
       render nothing: true, status: :bad_request
 		end
     
+  end
+  
+  def destroy
+    @note.delete
+    if @note.save
+        render json: @note
+    else
+        render nothing: true, status: :bad_request
+    end
+  end
+  
+  private
+  def find_note
+    @note = Note.create(params[:note])
   end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -12,6 +12,14 @@ RSpec.describe Api::V1::NotesController, type: :controller do
     note_json = {description: "Starting the test",project_id: @project.id}
     post :create, project_id: @project.id, note: note_json
     expect(response.status).to eq 200
-
+  end
+  
+  it "delete a project" do
+    FactoryGirl.create :note, description: 'Use your illusion 1', project_id: @project.id, id: 1
+    delete :destroy, project_id: @project.id, id: 1
+  
+    expect(response.status).to eq 200
+    body = JSON.parse(response.body)
+    expect(body['state']).to eq('archived')
   end
 end
